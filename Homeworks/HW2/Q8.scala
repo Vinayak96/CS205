3f8628f3c54b5e54928dae6ed82826575f032067
Vinayak Agarwal <Vinayak Agarwal>
2016-10-04 21:31:24 +0530

    Q8 rewritten

--- Homeworks/HW2/Q8.scala
+++ Homeworks/HW2/Q8.scala
@@ -1,20 +1,21 @@
 object Q8 {
-  def interChange(x:Int, l:List[Int]):List[List[Int]] = l match { 
-    case Nil => List(List(x))
-    case (head::tail) => (x :: head :: tail) :: interChange(x, tail).map(head :: _)
-}
-  
-
-  def permutationsGen(l:List[Int]):List[List[Int]] = l match {
-    case Nil => List(List())
-    case (head::tail) => for(p0 <- permutationsGen(tail); p1 <- interChange(head, p0)) yield p1
-}
-  def main(args: Array[String]): Unit = {
-    var lst = List(1,2,3)
-    println(permutationsGen(lst).distinct)
-   
+  def rotate(x:List[Int],l:Int): List[List[Int]]= {
+    if (x==Nil)
+      return Nil
+    else if(l==1)
+      return List(x)
+    else
+      return x :: rotate( (x.last :: x.init),l-1)
+  }
+  def Permutate(x:List[Int]):List[List[Int]]={
+    def helper(x:List[Int],z:Int):List[List[Int]]={
+      var y = rotate(x,x.length)
+      if (z==0)
+        return Nil
+      else
+        return (rotate(y.head.tail, y.head.tail.length).map(_ :+y.head.head))::: helper(y.last,z-1)
+  }
+    return helper(x,x.length)
     }
     
-    
-   
-  }
+ }
