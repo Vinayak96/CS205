90822ae18f233c155ed2fd2a8202f1698b71440d
Shivangi Tikekar <shivz@live.in>
2016-12-10 16:36:47 +0530

    Removed dots from event card

--- Project/mysite/yellow_line/urls.py
+++ Project/mysite/yellow_line/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'displaydate.html',views.displaydate,name='displaydate'),
     url(r'displayvenue.html',views.displayvenue,name='displayvenue'),
     url(r'displaycategory.html',views.displaycategory,name='displaycategory'),
-    url(r'(?P<a>[\w ]+).html',views.searchcategory,name='searchcategory')
+    url(r'(?P<a>[\w ]+).html',views.searchcategory,name='searchcategory'),
+    url(r'^signup/$', views.signup, name='signup'),
     ]
