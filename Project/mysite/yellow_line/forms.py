588c22c8b1a93cae5ad5bbe41fa4f5d18adb9328
Vinayak Agarwal <Vinayak Agarwal>
2016-12-13 17:20:41 +0530

    login isuue solved

--- Project/mysite/yellow_line/forms.py
+++ Project/mysite/yellow_line/forms.py
@@ -8,7 +8,7 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'first_name', 'last_name', 'email','password')
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
